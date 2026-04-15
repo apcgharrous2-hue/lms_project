6a4c66fe7905d688cc168dbdced843e1c1551faa
apcgharrous2-hue <apcgharrous2@gmail.com>
2026-04-15 14:34:39 +0200

    Update urls.py

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -11,18 +11,6 @@ urlpatterns = [
     # الصفحة الرئيسية
     path('', home_redirect),
 
-    # تطبيق courses
+    # كل شيء داخل app courses
     path('', include('courses.urls')),
-path('login/', login_view, name='login'),
-path('logout/', logout_view, name='logout'),
-path('login/', login_view),
-path('logout/', logout_view),
-
-path('courses/', course_list, name='courses'),
-path('course/<int:course_id>/', course_detail),
-path('enroll/<int:course_id>/', enroll_course),
-path('unenroll/<int:course_id>/', unenroll_course),
-path('my-courses/', my_courses),
-path('exam/<int:course_id>/', course_exam),
-path('certificate/<int:course_id>/', certificate),
 ]
