97ea3a1a72cdc78e5108bbffcb90f9b126d8d3cc
apcgharrous2-hue <apcgharrous2@gmail.com>
2026-04-16 12:49:34 +0200

    Update urls.py

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, include
 from django.shortcuts import redirect
 
 def home_redirect(request):
-    return redirect('courses/')  # أو dashboard حسب مشروعك
+    return redirect('/courses/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', home_redirect),
 
-    path('courses/', include('courses.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('', include('courses.urls')),
+    path('', include('accounts.urls')),  # 👈 مهم جدًا
 ]
