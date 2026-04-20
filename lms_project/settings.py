4616ef3086f288dfe34863c7b1061178a8e8173b
apcgharrous2-hue <apcgharrous2@gmail.com>
2026-04-20 10:32:28 +0200

    Update settings.py

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,10 +65,11 @@ WSGI_APPLICATION = 'lms_project.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
