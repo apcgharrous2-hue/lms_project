5eb877ff98ac8408cb8e0f3ac25d9481e1cbe4a4
khmegr <apcgharrous2@gmail.com>
2026-04-22 14:28:52 +0200

    إضافة مسار run-migrate

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def run_migrate(request):
+    import os
+    import django
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms_project.settings')
+    django.setup()
+    
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+    
+    return HttpResponse("✅ Migrations completed!")
 
 urlpatterns = [
+    path('run-migrate/', run_migrate),
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls')),
     path('', include('accounts.urls')),
