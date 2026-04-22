625289f18c0506e4e5b7629957a67dcc06076400
khmegr <apcgharrous2@gmail.com>
2026-04-22 13:32:48 +0200

    إضافة مسار مؤقت لتشغيل migrate

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.core.management import call_command
+from django.http import HttpResponse
+
+def run_migrate(request):
+    call_command('migrate', interactive=False)
+    return HttpResponse("✅ Migrations done!")
 
 urlpatterns = [
+    path('run-migrate/', run_migrate),
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls')),
     path('', include('accounts.urls')),
