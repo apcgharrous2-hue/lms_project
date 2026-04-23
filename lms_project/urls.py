f98e31d9444fcd0a27629738673b2b3c5a359100
khmegr <apcgharrous2@gmail.com>
2026-04-23 13:59:08 +0200

    إضافة مسار my-courses

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from courses.views import my_courses  # ✅ استيراد الدالة
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls')),
+    path('my-courses/', my_courses, name='my_courses'),  # ✅ أضف هذا
     path('', include('accounts.urls')),
 ]
